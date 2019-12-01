13084a3667d1e62afb8664c23cb9d1f57fa6342f
wasikm04 <maciej.wasik1@gmail.com>
2019-12-01 11:20:53 +0100

    changed profiles

--- src/main/java/pl/medical/service/files/configurations/MongoDBConfiguration.java
+++ src/main/java/pl/medical/service/files/configurations/MongoDBConfiguration.java
@@ -1,17 +1,20 @@
 package pl.medical.service.files.configurations;
 
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 @Configuration
 @EnableMongoRepositories("pl.medical.service.files.repositories")
-public class MongoDBConfiguration extends AbstractMongoConfiguration {
+public class MongoDBConfiguration extends AbstractMongoClientConfiguration {
 
     @Value("${spring.data.mongodb.uri}")
     private String mongoUri;
@@ -24,11 +27,12 @@ public class MongoDBConfiguration extends AbstractMongoConfiguration {
     @Override
     @Bean
     public MongoClient mongoClient() {
-        return new MongoClient(new MongoClientURI(mongoUri));
+        return MongoClients.create(mongoUri);
     }
 
     @Override
     protected String getDatabaseName() {
         return "Files";
     }
+
 }
